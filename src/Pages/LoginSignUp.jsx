0f7693ed330790eb542b2bdbcebff8f0d7e6b7bf
Aditi Upadhyaya <aditi732003@gmail.com>
2024-01-26 19:44:58 +0530

    Update LoginSignUp.jsx
    testing my commit

--- src/Pages/LoginSignUp.jsx
+++ src/Pages/LoginSignUp.jsx
@@ -4,7 +4,7 @@ import './CSS/LoginSignUp.css'
 export const LoginSignUp = () => {
   return <div className="loginsignup">
     <div className="loginsignup-container">
-      <h1>Sign Up</h1>
+      <h1>Sign Up!!</h1>
       <div className="loginsignup-fields">
         <input type="text" placeholder='Your Name' />
         <input type="email" placeholder='Email Address'/>
